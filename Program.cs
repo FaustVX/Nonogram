9946103d187eb7150306c4dcdd54652b9c357133
FaustVX <jodu035@gmail.com>
2021-06-22 04:13:06 +0200

    Refactored tests

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,50 +9,82 @@ namespace NonogramRow
     [TestClass]
     public class Program
     {
+        private readonly Random rng = new();
+
+        private bool[] GenerateRow(int[] groups, bool trimmStart, bool trimmEnd)
+        {
+            var row = trimmStart ? Enumerable.Empty<bool>() : Enumerable.Repeat(false, GetRng());
+            foreach (var group in groups[trimmEnd ? ..^1 : ..^0])
+                row = row.Concat(Enumerable.Repeat(true, group)).Concat(Enumerable.Repeat(false, GetRng()));
+            if (trimmEnd)
+                row = row.Concat(Enumerable.Repeat(true, groups[^1]));
+            return row.ToArray();
+        }
+
+        private int GetRng()
+            => rng.Next(1, 5);
+
         [TestMethod]
         public void GeneratedRows()
         {
-            Random rng = new();
 
             for (int i = GetRng() - 1; i >= 0 ; i--)
             {
                 var groups = Enumerable.Range(0, GetRng()).Select(_ => GetRng()).ToArray();
-                var row = GenerateRow(groups);
+                var row = GenerateRow(groups, false, false);
                 var generated = CalculateGroups(row);
                 CollectionAssert.AreEqual(groups, generated, $"Iteration {i}");
             }
+        }
 
-            bool[] GenerateRow(int[] groups)
+        [TestMethod]
+        public void GeneratedRowsTrimmedStart()
             {
-                var row = Enumerable.Repeat(false, GetRng());
-                foreach (var group in groups)
-                    row = row.Concat(Enumerable.Repeat(true, group)).Concat(Enumerable.Repeat(false, GetRng()));
-                return row.ToArray();
+            for (int i = GetRng() - 1; i >= 0 ; i--)
+            {
+                var groups = Enumerable.Range(0, GetRng()).Select(_ => GetRng()).ToArray();
+                var row = GenerateRow(groups, true, false);
+                var generated = CalculateGroups(row);
+                CollectionAssert.AreEqual(groups, generated, $"Iteration {i}");
+            }
             }
 
-            int GetRng()
-                => rng.Next(1, 5);
+        [TestMethod]
+        public void GeneratedRowsTrimmedEnd()
+        {
+            for (int i = GetRng() - 1; i >= 0 ; i--)
+            {
+                var groups = Enumerable.Range(0, GetRng()).Select(_ => GetRng()).ToArray();
+                var row = GenerateRow(groups, false, true);
+                var generated = CalculateGroups(row);
+                CollectionAssert.AreEqual(groups, generated, $"Iteration {i}");
+            }
+        }
+
+        [TestMethod]
+        public void GeneratedRowsTrimmed()
+        {
+            for (int i = GetRng() - 1; i >= 0 ; i--)
+            {
+                var groups = Enumerable.Range(0, GetRng()).Select(_ => GetRng()).ToArray();
+                var row = GenerateRow(groups, true, true);
+                var generated = CalculateGroups(row);
+                CollectionAssert.AreEqual(groups, generated, $"Iteration {i}");
+            }
         }
 
         [TestMethod]
         public void EmptyGroup()
         {
-            Random rng = new();
-
             var groups = Array.Empty<bool>();
             var row = Enumerable.Repeat(false, GetRng()).ToArray();
             var generated = CalculateGroups(row);
             CollectionAssert.AreEqual(groups, generated);
-
-            int GetRng()
-                => rng.Next(1, 5);
         }
 
         [TestMethod]
         public void FailWithStuckGroups()
         {
-            Random rng = new();
-
             var groups = new[] { 1, 1 };
             var row = new[] { false, true, true, false };
             var generated = CalculateGroups(row);
