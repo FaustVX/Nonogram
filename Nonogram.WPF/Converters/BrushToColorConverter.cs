172055412a4d6647826e618c3094414b83965c4f
FaustVX <jodu035@gmail.com>
2021-07-06 07:44:14 +0200

    Added some Style

---
+++ Nonogram.WPF/Converters/BrushToColorConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Nonogram.WPF.Converters
+{
+    public class BrushToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is SolidColorBrush brush ? brush.Color : value;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
