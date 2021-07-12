e38883de30288047b202e5b1c5bd98e75ff87dd8
FaustVX <jodu035@gmail.com>
2021-07-12 22:42:06 +0200

    Added Zoom

--- Nonogram.WPF/Converters/IMultiValueConverter.cs
+++ Nonogram.WPF/Converters/IMultiValueConverter.cs
@@ -17,6 +17,19 @@ namespace Nonogram.WPF.Converters
             => throw new NotImplementedException();
     }
 
+    public interface IMultiValueConverterParametered<T1, T2, TParam, TOut> : IMultiValueConverter
+    {
+        TOut Convert((T1, T2) values, TParam param);
+
+        object? IMultiValueConverter.Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+            => values.Length is 2 && values[0].CheckType<T1>(true) is var t1 && values[1].CheckType<T2>(true) is var t2 && parameter.CheckType<TParam>(true) is var param
+            ? Convert((t1, t2), param)
+            : throw new Exception();
+
+        object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
     public interface IMultiValueConverter<T1, T2, T3, TOut> : IMultiValueConverter
     {
         TOut Convert((T1, T2, T3) values);
