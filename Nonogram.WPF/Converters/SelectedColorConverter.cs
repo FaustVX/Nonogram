225b006fa2c82ad526173fa4f52c3cebd9f7a895
FaustVX <jodu035@gmail.com>
2021-07-25 13:42:54 +0200

    Grid border color bind to current color

---
+++ Nonogram.WPF/Converters/SelectedColorConverter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace Nonogram.WPF.Converters
+{
+    public class SelectedColorConverter : IMultiValueConverter<IList<Game<Brush>.Color>, int, Brush>
+    {
+        public Brush Convert((IList<Game<Brush>.Color>, int) values)
+            => values.Item1?[values.Item2].Value;
+    }
+}
